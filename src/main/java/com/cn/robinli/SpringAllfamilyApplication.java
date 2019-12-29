2bed246df752fad656317b7c9618221e912a96ca
robinli <lixuegen_vip@163.com>
2019-12-29 15:23:00 +0800

    初始化

---
+++ src/main/java/com/cn/robinli/SpringAllfamilyApplication.java
@@ -0,0 +1,14 @@
+package com.cn.robinli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllfamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAllfamilyApplication.class, args);
+		System.out.println("启动成功！");
+	}
+
+}
---
+++ src/test/java/com/cn/robinli/SpringAllfamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.robinli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllfamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
